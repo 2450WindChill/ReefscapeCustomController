2157b08a48c71358c79feb38f7f1323b9722eb52
Mikael Coenen <coenenmikael@gmail.com>
2025-01-25 13:20:40 -0600

    LEDS now turn on with computer inputs!

--- serial_test.py
+++ serial_test.py
@@ -1,10 +1,19 @@
 import serial
 import time
 
-ser = serial.Serial('COM3')
+ser = serial.Serial('COM4')
+ # turns light on for 5 secs, shuts off after
 
-data = 1
-ser.write(data)
+ # sets data to 1, returns value
+data = "1 \r\n"
+print("Writing 1")
+# writes data to controller, prints to terminal
+print(ser.write(bytearray(data, 'ascii')))
+
 time.sleep(5)
-data = 0
-ser.write(data)
+
+# sets data to 0, returns value
+data = "0 \r\n"
+print("Writing 0")
+# writes data to controller, prints to terminal
+print(ser.write(bytearray(data, 'ascii')))
