53b570c952245b8de808ba6970e1bcbb94fe3874
Mikael Coenen <103543538+IsaacVanOosterom@users.noreply.github.com>
2025-02-08 14:19:19 -0600

    made controller LEDS work
    desc. in code comments

--- Pico-gamepad/code.py
+++ Pico-gamepad/code.py
@@ -10,7 +10,9 @@ import board
 import digitalio
 import analogio
 import usb_hid
-
+import supervisor
+import time
+from digitalio import DigitalInOut, Direction, Pull
 from hid_gamepad import Gamepad
 
 gp = Gamepad(usb_hid.devices)
@@ -29,6 +31,7 @@ bonk2Gamepad = 4
 
 HookPin = board.GP2
 HookPinGamepad = 5
+ActionLED = board.GP27
 
 L1Pin = board.GP22
 L1Gamepad = 6
@@ -44,24 +47,31 @@ L3LGamepad = 9
 
 L3RPin = board.GP19
 L3RGamepad = 10
+ScoringLED = board.GP28
 
 RF1Pin = board.GP7
 RF1Gamepad = 11
+RF1Led = board.GP12
 
 RF2Pin = board.GP8
 RF2Gamepad = 12
+RF2Led = board.GP11
 
 RF3Pin = board.GP17
 RF3Gamepad = 13
+RF3Led = board.GP15
 
 RF4Pin = board.GP16
 RF4Gamepad = 14
+RF4Led = board.GP26
 
 RF5Pin = board.GP9
 RF5Gamepad = 15
+RF5Led = board.GP14
 
 RF6Pin = board.GP10
 RF6Gamepad = 16
+RF6Led = board.GP13
 
 
 
@@ -82,7 +92,7 @@ button_pins = ( upPin,
                 RF3Pin,
                 RF4Pin,
                 RF5Pin,
-                RF6Pin)
+                RF6Pin )
 
 # Map the buttons to button numbers on the Gamepad.
 # gamepad_buttons[i] will send that button number when buttons[i]
@@ -102,7 +112,17 @@ gamepad_buttons = ( upGamepad,
                     RF3Gamepad,
                     RF4Gamepad,
                     RF5Gamepad,
-                    RF6Gamepad)
+                    RF6Gamepad )
+
+# The pins connected to each LED
+ledpins = ( ActionLED,
+           ScoringLED,
+           RF1Led,
+           RF2Led,
+           RF3Led,
+           RF4Led,
+           RF5Led,
+           RF6Led )
 
 buttons = [digitalio.DigitalInOut(pin) for pin in button_pins]
 for button in buttons:
@@ -117,7 +137,62 @@ def range_map(x, in_min, in_max, out_min, out_max):
     return (x - in_min) * (out_max - out_min) // (in_max - in_min) + out_min
 
 
+leds = []
+
+for pin in ledpins:
+    led = DigitalInOut(pin)
+    led.direction = Direction.OUTPUT
+    leds.append(led)
+
+# Action LEDS are always on, Scoring LEDS only turn on when at least one RF LED is on, RF LEDS turn on via networktables
+leds[0].value = True
 while True:
+    if (leds[2].value == True) or (leds[3].value == True) or (leds[4].value == True) or (leds[5].value == True) or (leds[6].value == True) or (leds[7].value == True):
+        leds[1].value = True
+    else:
+        leds[1].value = False
+
+    if supervisor.runtime.serial_bytes_available:
+        value = input().strip()
+        if value == "":
+            continue
+        print(value)
+        if value == "RF1on":
+            leds[2].value = True
+        if value == "RF1off":
+            leds[2].value = False
+
+        if value == "RF2on":
+            leds[3].value = True
+        if value == "RF2off":
+            leds[3].value = False
+
+        if value == "RF3on":
+            leds[4].value = True
+        if value == "RF3off":
+            leds[4].value = False
+
+        if value == "RF4on":
+            leds[5].value = True
+        if value == "RF4off":
+            leds[5].value = False
+        
+        if value == "RF5on":
+            leds[6].value = True
+        if value == "RF5off":
+            leds[6].value = False
+        
+        if value == "RF6on":
+            leds[7].value = True
+        if value == "RF6off":
+            leds[7].value = False
+
+    # for led in range(len(ledpins)):
+    #     switch_LED = leds[led]
+    #     switch_LED.value = True
+    #     time.sleep(1)
+    #     switch_LED.value = False
+    
     # Buttons are grounded when pressed (.value = False).
     for i, button in enumerate(buttons):
         gamepad_button_num = gamepad_buttons[i]
--- references/serial_test.py
+++ references/serial_test.py
@@ -1,19 +1,35 @@
 import serial
 import time
 
-ser = serial.Serial('COM4')
+ser = serial.Serial('COM16')
  # turns light on for 5 secs, shuts off after
 
- # sets data to 1, returns value
-data = "1 \r\n"
-print("Writing 1")
-# writes data to controller, prints to terminal
-print(ser.write(bytearray(data, 'ascii')))
+#  # sets data to 1, returns value
+# data = "RF1on \r\n"
+# print("Writing RF1on")
+# # writes data to controller, prints to terminal
+# print(ser.write(bytearray(data, 'ascii')))
 
-time.sleep(5)
+# time.sleep(5)
 
-# sets data to 0, returns value
-data = "0 \r\n"
-print("Writing 0")
-# writes data to controller, prints to terminal
-print(ser.write(bytearray(data, 'ascii')))
+# # sets data to 0, returns value
+# data = "RF1off \r\n"
+# print("Writing RF1off")
+# # writes data to controller, prints to terminal
+# print(ser.write(bytearray(data, 'ascii')))
+
+# goes through each RF and turns on the LED, and then goes to a nonexistent RF to turn everything off for one round
+while True:
+    x = 1
+    while x <= 7:
+        data = "RF" + str(x) + "on \r\n"
+        print(str(x) + " on")
+        print(ser.write(bytearray(data, 'ascii')))
+
+        time.sleep(2)
+
+        data = "RF" + str(x) + "off \r\n"
+        print(str(x) + " off")
+        print(ser.write(bytearray(data, 'ascii')))
+
+        x += 1
